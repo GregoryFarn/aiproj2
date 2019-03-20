a074e6f13425365aa2c2b63a920144d8bfafaeb3
GregoryFarn <farn@usc.edu>
2019-03-19 23:48:13 -0700

    asdf

---
+++ Project2/project2cs360s2019.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+
+int main(int argc, const char* argv[]) {
+
+	std::ifstream file("input.txt", std::ifstream::in);
+	std::string temp;
+	int hNum;
+	std::string searchType;
+
+
+	file >> hNum;
+	file >> searchType;
+
+
+		return 1;
+}
