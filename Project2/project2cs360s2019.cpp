ccce604166d37b0cd80d59a7c1ac0db153cc1a8b
GregoryFarn <farn@usc.edu>
2019-03-21 14:17:24 -0700

    asdflk

--- Project2/project2cs360s2019.cpp
+++ Project2/project2cs360s2019.cpp
@@ -7,6 +7,8 @@
 #include <algorithm>
 
 struct hero{
+	hero() {
+	}
 	hero(int id_,
 		double pow_,
 		double teaMast_,
@@ -16,7 +18,24 @@ struct hero{
 		pow(pow_),
 		teaMast(teaMast_),
 		oppMast(oppMast_),
-		memb(memb_){}
+		memb(memb_) {}
+
+	/*hero(hero& x) :
+		id(x.id),
+		pow(x.pow),
+		teaMast(x.teaMast),
+		oppMast(x.oppMast),
+		memb(x.memb) {}
+
+	hero operator=(hero& x) :
+		id(x.id),
+		pow(x.pow),
+		teaMast(x.teaMast),
+		oppMast(x.oppMast),
+		memb(x.memb) {
+	return *this
+	}*/
+
 	int id;
 	double pow;
 	double teaMast;
@@ -24,15 +43,68 @@ struct hero{
 	int memb;
 };
 
-struct node {
-	double value;
-	hero choice;
-	node* parent;
-	std::vector<node*> children;
+struct treeNode {
+	treeNode() :
+		val(0),
+		choice(nullptr),
+		par(nullptr)
+	{
+	};
+	treeNode(double val_,hero* choice_,treeNode* par_) :
+	val(val_),
+	choice(choice_),
+	par(par_)
+	{
+	};
+	double val;
+	hero *choice;
+	treeNode* par;
+	std::vector<treeNode*> children;
 };
 
-int main(int argc, const char* argv[]) {
+int findMax(std::vector<treeNode*> child){
+	int max = 0;
+	for (int i = 0; i < child.size(); i++) {
+		if (child[i]->val > max) {
+			max = i;
+		}
+}
+	return max;
+}
 
+int findMin(std::vector<treeNode*> child) {
+	int min = 1000;
+	for (int i = 0; i < child.size(); i++) {
+		if (child[i]->val < min) {
+			min = i;
+		}
+	}
+	return min;
+}
+void minMaxTree(std::vector<hero*> unchose,int level, treeNode* parent,bool oppTea) {
+	if (level == 0) {
+		parent->children = {};
+		if (oppTea) {
+			parent->val = parent->choice->pow * parent->choice->teaMast;
+		}
+		else {
+			parent->val = parent->choice->pow * parent->choice->oppMast;
+		}
+		return;
+	}
+	std::vector<treeNode*> child;
+	for (int i = 0; i < unchose.size(); i++) {
+		std::vector<hero*> unchoseTemp(unchose);
+		child.push_back(new treeNode(-1, unchose[i], parent));
+		unchoseTemp.erase(unchoseTemp.begin() + i);
+		minMaxTree(unchoseTemp, level - 1, child[i],!oppTea);
+	}
+	parent->children = child;
+}
+
+
+
+int main(int argc, const char* argv[]) {
 	std::ifstream file("input0.txt", std::ifstream::in);
 	std::string temp;
 	int hNum;
@@ -60,7 +132,7 @@ int main(int argc, const char* argv[]) {
 			unchose.push_back(hero(id, pow, teaMast, oppMast, memb));
 		}
 		else if (memb == 1) {
-
+			numChose++;
 		}
 	}
 
@@ -68,9 +140,14 @@ int main(int argc, const char* argv[]) {
 		return a.id < b.id;
 	});
 
-		return 1;
-}
-
-int minMax(std::vector<hero> unchose) {
+	std::vector<hero*> unchosePoint;
 	
+	std::transform(unchose.begin(), unchose.end(), std::back_inserter(unchosePoint), [](hero &a) {
+		return &a;
+	});
+
+	treeNode * root = new treeNode();
+	minMaxTree(unchosePoint, (5-numChose) *2,root,true);
+	std::cout << "blek\n";
+		return 1;
 }
